00cefbf2a895f5fb8ebdd7b76f88919c0f469db2
jamdhz <162110165+jamdhz@users.noreply.github.com>
2024-07-24 18:21:02 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('spin-btn').addEventListener('click', function() {
-    const wheel = document.getElementById('wheel');
-    const degree = Math.floor(5000 + Math.random() * 5000);
-    
-    wheel.style.transform = `rotate(${degree}deg)`;
-});
